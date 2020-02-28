4983066e71d57b4990ca130719e2d6b3e8104b75
M-Zayn <59746872+M-Zayn@users.noreply.github.com>
2020-02-28 20:54:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,25 @@ $( document ).ready(function() {
     
 });
 
+
+$(window).on("scroll", function(){
+
+  $('a[data-scroll]').click(function (e) {
+    e.preventDefault();
+    //Set Offset Distance from top to account for fixed nav
+    var offset = 80;
+    var target = ('#' + $(this).data('scroll'));
+    var $target = $(target);
+    //Animate the scroll to, include easing lib if you want more fancypants easings
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - offset
+    }, 1000);
+  });
+
+});
+
+
+
   $('.sec-three .owl-carousel').owlCarousel({
     loop:false,
     margin:0,
